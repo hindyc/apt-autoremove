b49edddc7fb8bb1c5c966a12333f68ac69ff6eab
Chris Hindy <chindy@lwpca.net>
2015-08-17 09:30:17 -0400

    return 16 if we make a change.  trigger changed attribute accordingly.

--- roles/autoremove/files/dpkg-rc.sh
+++ roles/autoremove/files/dpkg-rc.sh
@@ -2,9 +2,11 @@
 
 PURGELIST=`dpkg --list |grep "^rc" | cut -d " " -f 3`
 LISTLEN=${#PURGELIST}
+RETURN=0
 
 if [ $LISTLEN -gt 0 ]; then
     echo $PURGELIST | xargs dpkg --purge
+    RETURN=16
 fi
 
-exit 0
+exit $RETURN
